4f45eda6f47deba510977dea01439cd8bbdeb5f4
Geeoon Chung <geeoon13@gmail.com>
2020-07-02 15:05:02 -0500

    Finished

--- project/bean/bean.ino
+++ project/bean/bean.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+#define MOTOR_PIN 25
 
+unsigned long DELAY_START = 0; //0 ms
+unsigned long WATERING_TIME = 10000; //3 seconds in ms
+unsigned long WATERING_DELAY = 10800000; //3 hours in ms
+
+void waterFor(unsigned long wateringT);
+
+void setup() {
+  pinMode(MOTOR_PIN, OUTPUT);
+  digitalWrite(MOTOR_PIN, LOW); //turn of watering
+  DELAY_START = millis();
+  waterFor(WATERING_TIME); //start initial watering process
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (millis() - DELAY_START >= WATERING_DELAY) {
+    waterFor(WATERING_TIME);
+    DELAY_START = millis();
+  }
+}
 
+void waterFor(unsigned long wateringT) {//wateringT is in ms
+  unsigned long start = millis();
+  while (millis() - start < wateringT) {
+    digitalWrite(MOTOR_PIN, HIGH);
+  }
+  digitalWrite(MOTOR_PIN, LOW);
 }
